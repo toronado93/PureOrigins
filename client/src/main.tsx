aa8e15357689c5858d056c4a92b27532e9a22eb1
byuksel96 <beyuksel96@gmail.com>
2026-01-30 15:09:17 +0000

    added vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 import "./tailwind.css";
 import "./styles.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
